dfa90ac06aab1a56320e2ef69334e0eb300daed9
ZhuJD-China <980708354@qq.com>
2024-12-31 17:33:23 +0800

    remove winreg

--- Rainbow_utils/get_google_result.py
+++ Rainbow_utils/get_google_result.py
@@ -6,7 +6,6 @@ import urllib.parse
 import urllib.request
 import requests
 from bs4 import BeautifulSoup
-import winreg
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
